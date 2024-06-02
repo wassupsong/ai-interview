ec5e0b6122fb394fcc3b098f1a3a8c0adc831574
wassupsong <shs0783@gmail.com>
2024-06-02 23:39:20 +0900

    [fix] response dto

--- src/analyze/dto/response-analyze.ts
+++ src/analyze/dto/response-analyze.ts
@@ -1,15 +1,28 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDateString, IsNumber, IsObject, IsString } from "class-validator";
+import {
+  IsDateString,
+  IsNumber,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from "class-validator";
 import { ReqAnalyzeDto } from "./request-analyze";
+import { Type } from "class-transformer";
 
-interface Feedback {
-  score: number;
-  comment: string;
+class Feedback {
+  @IsNumber()
+  readonly score: number;
+
+  @IsString()
+  readonly comment: string;
 }
+
 export class ResAnalyzeDto {
-  @IsObject()
+  @Type(() => Feedback)
+  @ValidateNested()
   readonly decibel_feedback: Feedback;
 
-  @IsObject()
+  @Type(() => Feedback)
+  @ValidateNested()
   readonly pitch_feedback: Feedback;
 }
