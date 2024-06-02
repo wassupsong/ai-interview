c4137c38d430d603caeb2e5ef1fad1042947e0b7
wassupsong <shs0783@gmail.com>
2024-06-02 14:59:47 +0900

    [feat] 분석서버 통신 및 sse

--- src/analyze/analyze.controller.ts
+++ src/analyze/analyze.controller.ts
@@ -1,21 +1,57 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, MessageEvent, Post, Sse } from "@nestjs/common";
 import { ReqAnalyzeDto } from "./dto/request-analyze";
 import { AnalyzeService } from "./analyze.service";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Test } from "./dto/test";
+import { ResAnalyzeDto } from "./dto/response-analyze";
+import { Observable, Subject, interval, map } from "rxjs";
 
 @Controller("analyze")
 @ApiTags("분석 서버 연결 api")
 export class AnalyzeController {
   constructor(private readonly analyzeService: AnalyzeService) {}
+  private sseSubject: Subject<MessageEvent> = new Subject();
 
   @Post("")
   @ApiOperation({
     summary: "음성 데이터 분석 api(app to server)",
     description: "분석 서버로 pcm 데이터 전달",
   })
+  @ApiResponse({
+    status: 201,
+    description: "success",
+    type: ResAnalyzeDto,
+  })
   async postData(@Body() body: ReqAnalyzeDto) {
     return this.analyzeService.sendPcmData(body);
+  }
+
+  @Post("result")
+  @ApiOperation({
+    summary: "분석된 데이터 전달",
+    description: "분석 데이터 앱으로 푸시",
+  })
+  @ApiResponse({
+    status: 201,
+    description: "success",
+  })
+  async receiveData(@Body() body: ResAnalyzeDto) {
+    console.log("분석 자료 확인");
+    console.log(this.sseSubject);
+    if (this.sseSubject.closed) {
+      console.log("SSE 닫혀있음");
+    }
+    this.sseSubject.next(this.analyzeService.receiveData(body));
+    return true;
+  }
+
+  @Sse("sse")
+  @ApiOperation({
+    summary: "클라이언트 sse 연결",
+  })
+  sse(): Observable<MessageEvent> {
+    console.log("클라이언트 연결 시도");
+    return this.sseSubject.asObservable();
   }
 
   @Post("test")
@@ -25,11 +61,5 @@ export class AnalyzeController {
       return true;
     }
     return false;
-  }
-
-  @Get("getTest")
-  getTest() {
-    console.log("getTest success");
-    return "재환이형 ? 들린다면 카톡해";
   }
 }
--- src/analyze/analyze.service.ts
+++ src/analyze/analyze.service.ts
@@ -1,8 +1,12 @@
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  MessageEvent,
+} from "@nestjs/common";
 import { ReqAnalyzeDto } from "./dto/request-analyze";
 import { ConfigService } from "@nestjs/config";
-import { map } from "rxjs";
 import { AxiosResponse } from "axios";
 import { ResAnalyzeDto } from "./dto/response-analyze";
 
@@ -16,12 +20,29 @@ export class AnalyzeService {
   async sendPcmData(
     body: ReqAnalyzeDto,
   ): Promise<AxiosResponse<ResAnalyzeDto>> {
-    const url = this.configService.get<string>("ANALYZE_API_URL");
-    console.log(body);
-    console.log(url);
+    try {
+      const url = this.configService.get<string>("ANALYZE_API_URL");
+      const result = await this.httpService.axiosRef.post(
+        `${url}/hj_test`,
+        body,
+      );
+      if (result.data) {
+        return result.data;
+      } else {
+        throw new HttpException("failed to send pcm data", 1000);
+      }
+    } catch (error) {
+      throw new HttpException(
+        "Flask Server Error",
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-    const result = await this.httpService.axiosRef.post(`${url}/hj_test`, body);
-
-    return result.data;
+  receiveData(body: ResAnalyzeDto): MessageEvent {
+    console.log("분석 자료 sse 전달");
+    return {
+      data: body,
+    };
   }
 }
