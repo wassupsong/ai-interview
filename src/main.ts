6679d730e92760b60ba8b445dce5659b7f8b0b3a
wassupsong <shs0783@gmail.com>
2024-06-02 23:38:44 +0900

    [fix] http limit size

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { setSwagger } from "./util/SetSwagger";
+import { json, urlencoded } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: "50mb" }));
+
+  app.use(urlencoded({ extended: true, limit: "50mb" }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
